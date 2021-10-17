fixed gradient error in matrix product implementation

chiF_Hv_product/CG.py
@@ -5,7 +5,7 @@ from functools import partial
 
 
 
-def CG_Algorithm(A, b, initial_x, sparse=False):
+def CG_Algorithm(g,A, b, initial_x, sparse=False):
     """
     Solve for x in linear equation Ax=b
     with an initial choose of x, initial_x.
@@ -31,28 +31,28 @@ def CG_Algorithm(A, b, initial_x, sparse=False):
     n = b.shape[0]
     epsilon = 1e-7
     x = initial_x
-    r = b - Amap(x)
+    r = b - Amap(g,x)
 
     if(np.linalg.norm(r) < epsilon):
         return x
 
     d = r
-    alpha = np.matmul(r, r) / np.matmul(Amap(d),d)
+    alpha = np.matmul(r, r) / np.matmul(Amap(g,d),d)
 
     for i in range(n):
         x = x + alpha * d
-        r_next = r - alpha * Amap(d)
+        r_next = r - alpha * Amap(g,d)
         if(np.linalg.norm(r_next) < epsilon):
             break
         beta = np.matmul(r_next,r_next) / np.matmul(r,r)
         r = r_next
         d = r + beta * d
-        alpha = np.matmul(r,r) / np.matmul(Amap(d), d)
+        alpha = np.matmul(r,r) / np.matmul(Amap(g,d), d)
     
     return x
 
 
-@partial(custom_vjp, nondiff_argnums=(0,1))
+@partial(custom_vjp, nondiff_argnums=(0,1,))
 def CGSubspaceSparse(Aadjoint_to_gadjoint, A, g, E_0, b, alpha):
     """
     Function primitive for low-rank CG linear
@@ -74,12 +74,10 @@ def CGSubspaceSparse(Aadjoint_to_gadjoint, A, g, E_0, b, alpha):
             linear system (A - E_0I)x = b in addition to
             the condition alpha^T x = 0.
     """
-    raise NotImplementedError("Sparse Implementation not finished!")
-
-    Aprime = lambda v: A(v) - E_0 * v
+    Aprime = lambda g,v: A(g,v) - E_0 * v
     initial_x = jnp.array(np.random.randn(b.shape[0]).astype(b.dtype))
     initial_x = initial_x - jnp.matmul(alpha,initial_x) * alpha
-    x = CG_Algorithm(Aprime,b,initial_x,sparse=True)
+    x = CG_Algorithm(g,Aprime,b,initial_x,sparse=True)
     return x
 
 def CGSubspaceSparse_fwd(Aadjoint_to_gadjoint, A, g, E_0, b, alpha):

chiF_Hv_product/Lanczos.py
@@ -3,7 +3,7 @@ import numpy as np
 from jax.config import config
 config.update("jax_enable_x64", True)
 
-def Lanczos(A, k, *, sparse=False, dim=None):
+def Lanczos(g,A, k, *, sparse=True, dim=None):
     """
     Lanczos method to help solve the eigenvalue
     and eigenvector of a real symmetrix matrix.
@@ -28,17 +28,17 @@ def Lanczos(A, k, *, sparse=False, dim=None):
         n = dim
         dtype = jnp.float64
         Amap = A
-    else:
-        n = A.shape[0]
-        dtype = A.dtype
-        Amap = lambda v: jnp.matmul(A, v)
+    # else:
+    #     n = A.shape[0]
+    #     dtype = A.dtype
+    #     Amap = lambda v: jnp.matmul(A, v)
     
     Q_k = jnp.zeros((n,k), dtype=dtype)
     alphas = jnp.zeros(k, dtype=dtype)
     betas = jnp.zeros(k - 1, dtype=dtype)
     q = jnp.array(np.random.randn(n).astype(dtype))
     q /= jnp.linalg.norm(q)
-    u = Amap(q)
+    u = Amap(g,q)
     alpha = jnp.matmul(q,u)
     Q_k = Q_k.at[:, 0].set(q)
     alphas = alphas.at[0].set(alpha)
@@ -53,7 +53,7 @@ def Lanczos(A, k, *, sparse=False, dim=None):
         qprime = q
         beta = jnp.linalg.norm(r)
         q = r / beta
-        u = Amap(q)
+        u = Amap(g,q)
         alpha = jnp.matmul(q,u)
         alphas = alphas.at[i].set(alpha)
         betas = betas.at[i-1].set(beta)
@@ -62,8 +62,8 @@ def Lanczos(A, k, *, sparse=False, dim=None):
     T = jnp.diag(alphas) + jnp.diag(betas, k=1) + jnp.diag(betas, k=-1)
     return Q_k, T
 
-def symeigLanczos(A, k, extreme="both", *,
-                    sparse=False, dim=None):
+def symeigLanczos(g,A, k, extreme="both", *,
+                    sparse=True, dim=None):
     """
         Computes the extreme eigenvalues and eigenvectors
     upon request of a symmetric matrix A with Lanczos
@@ -85,7 +85,7 @@ def symeigLanczos(A, k, extreme="both", *,
                 "max" -> max.                   --Output--> (eigval_max, eigvector_max)
     Output: See "Input" above.
     """
-    Q_k, T = Lanczos(A, k, sparse=sparse, dim=dim)
+    Q_k, T = Lanczos(g,A, k, sparse=sparse, dim=dim)
     eigvalsQ, eigvectorsQ = jnp.linalg.eigh(T,UPLO='U')
     eigvectorsQ = jnp.matmul(Q_k,eigvectorsQ)
     if extreme == "both":
@@ -97,38 +97,38 @@ def symeigLanczos(A, k, extreme="both", *,
 
 
 
-if __name__ == "__main__":
-    from TFIM_init import TFIM
+# if __name__ == "__main__":
+#     from TFIM_init import TFIM
 
-    N = 2
-    k = 3   
-    g = 1
-    model = TFIM(N,g)
+#     N = 2
+#     k = 3   
+#     g = 1
+#     model = TFIM(N,g)
 
 
-    model.setHmatrix()
-    H = model.Hmatrix
+#     model.setHmatrix()
+#     H = model.Hmatrix
     
-    print(f"H for N={N} is \n"
-            f"{H}\n")
-    print(f"Then testing Lanczos algorithm:\n")
+#     print(f"H for N={N} is \n"
+#             f"{H}\n")
+#     print(f"Then testing Lanczos algorithm:\n")
 
-    eigval_min, eigvector_min = symeigLanczos(H, k, extreme="min")
-    eigval_max, eigvector_max = symeigLanczos(H, k, extreme="max")
-    eigval_both_min, eigvector_both_min, eigval_both_max, eigvector_both_max = symeigLanczos(H, k, extreme="both")
+#     eigval_min, eigvector_min = symeigLanczos(H, k, extreme="min")
+#     eigval_max, eigvector_max = symeigLanczos(H, k, extreme="max")
+#     eigval_both_min, eigvector_both_min, eigval_both_max, eigvector_both_max = symeigLanczos(H, k, extreme="both")
 
-    print(f"param extreme = 'min', giving:\n"
-        f"eigval_min = {eigval_min}\n"
-        f"eigvector_min = {eigvector_min}\n")
-    print(f"param extreme = 'max', giving:\n"
-        f"eigval_max = {eigval_max}\n"
-        f"eigvector_max = {eigvector_max}\n")
-    print(f"param extreme = 'both', giving:\n"
-        f"eigval_both_min = {eigval_both_min}\n"
-        f"eigvector_both_min = {eigvector_both_min}\n"
-        f"eigval_both_max = {eigval_both_max}\n"
-        f"eigvector_both_max = {eigvector_both_max}\n")
+#     print(f"param extreme = 'min', giving:\n"
+#         f"eigval_min = {eigval_min}\n"
+#         f"eigvector_min = {eigvector_min}\n")
+#     print(f"param extreme = 'max', giving:\n"
+#         f"eigval_max = {eigval_max}\n"
+#         f"eigvector_max = {eigvector_max}\n")
+#     print(f"param extreme = 'both', giving:\n"
+#         f"eigval_both_min = {eigval_both_min}\n"
+#         f"eigvector_both_min = {eigvector_both_min}\n"
+#         f"eigval_both_max = {eigval_both_max}\n"
+#         f"eigvector_both_max = {eigvector_both_max}\n")
 
-    print("Test completed.")
+#     print("Test completed.")
 
     

chiF_Hv_product/TFIM_init.py
@@ -1,130 +1,75 @@
+import re
 from jax.config import config
 config.update("jax_enable_x64", True)
 import jax.numpy as jnp
 import numpy as np
 import jax
 
-class TFIM(object):
+def H_u_initialize(N):
     """
-        1D Transverse Field Ising Model(TFIM)
-        Hamiltonian:
-        H = - \sum_{i=0}^{N-1} (g \sigma_i^x + \sigma-i^z \sigma_{i+1}^z)
+    recieving the parameter g for TFIM model
+    and a vector u
+    and construct the Hamiltonian H,
+    return the product of H and u:H*u
+
+    Input: 'g': the parameter for TFIM model
+            'u': an (arbitrary) vector
+            'N': the number of sites of TFIM model
+    Output: 'result_H_u': result_H_u = H*u
+            'Hadjoint_to_gadjoint': the translation function
+                    to calculate gadjoint form
+                    Hadjoint
     """
-    # @partial(jit, static_argnums=(1,))
-    def __init__(self, N,g):
-        """
-        Initialization of the model
-        """
-        self.N = N
-        self.dim = 2**N
-        self.g = g
-        self._diags()
-        self._flips_basis()
-        # print(f"1D lattice size N={self.N} \n"
-        #     f"Model initialization completed.")
+    dim = 2**N
 
-    def _diags(self):
-        """
-        Deal with the diagonal elements of the Harmiltonian
-        """
-        indices = jnp.arange(self.dim)[:,jnp.newaxis]
-        bin_reps = (indices >> jnp.arange(self.N)[::-1]) & 1
-        spins = 1 - 2 * bin_reps
-        spins_prime = jnp.hstack( (spins[:,1:] , spins[:,0:1]) )
-        self.diag_elements = -(spins * spins_prime).sum(axis=1)
+    #initialize the diagnal of the hamiltonian matrix
+    diag_index = jnp.arange(dim)[:, jnp.newaxis]
+    diag_bin_reps = (diag_index >> jnp.arange(N)[::-1]) & 1
+    diag_spins = 1 - 2 * diag_bin_reps
+    diag_spins_prime = jnp.hstack((diag_spins[:,1:],diag_spins[:,0:1]))
+    diag_elements = -(diag_spins * diag_spins_prime).sum(axis=1)
 
-    def _flips_basis(self):
-        """
-        Deal with \sigma_i^x
-        """
-        masks = jnp.array([1 << i for i in range(self.N)], dtype="int64")
-        basis = jnp.arange(self.dim)[:,None]
-        self.flips_basis = basis ^ masks
+    #initialize the basis of flips
+    flip_masks = jnp.array([1 << i for i in range(N)], dtype="int64")
+    flip_basis = jnp.arange(dim)[:,None]
+    flips_basis = flip_basis ^ flip_masks
 
-    def setpHpg(self):
-        """
-        Return the direct matrix of
-        \partial H / \partial g
-        To be used in the full spectrum perturbation formula method
-        of calculating chi_F(fidelity susceptibility)
-        """
-        self.pHpgmatrix = jnp.zeros([self.dim,self.dim], dtype="float64")
-        self.pHpgmatrix = self.pHpgmatrix.at[self.flips_basis.T, jnp.arange(self.dim)].set(-1.0)
+    #first derivative of H, in a vector product form
+    def pHpg(v):
+        result_pHpg = -v[flips_basis].sum(axis=1)
+        return result_pHpg
 
-    def pHpg(self,v):
+    #defining the product function H
+    def H_u(g,u):
+        result_H_u = u * diag_elements - g * u[flips_basis].sum(axis=1)
+        return result_H_u
+    
+    def Hadjoint_to_gadjoint(v1,v2):
         """
-        Using equation (26):
-        pHpg = \partial H / \partial g
-            =  - \sum_{i=0}^{N-1} \sigma_i^x
-        """
-        resultv = -v[self.flips_basis].sum(axis=1)
-        return resultv
+        the adjoint translation function to be 
+        used in CG and symeig function
 
-    def setHmatrix(self):
+        Input: 'v1': one required vector to calculate A
+                'v2': another vector to calculate A
+                then A = v1 * v2^T(outer product)
+        Output: 'g_adjoint': the adjoint of the parameters(g)
+                    with respect to A
         """
-        Initialize the Hamiltonian 
-        To be stored in "self.Hmatrix"
-        """
-        diagmatrix = jnp.diag(self.diag_elements)
-        offdiagmatrix = jnp.zeros([self.dim,self.dim],dtype="float64")
-        offdiagmatrix = offdiagmatrix.at[self.flips_basis.T, jnp.arange(self.dim)].set(-self.g)
 
-        #to avoid devided by zero, add random noise into the Hamiltonian
-        randommatrix = 1e-12 * jnp.array(np.random.randn(self.dim, self.dim))
-        randommatrix = 0.5 * (randommatrix + randommatrix.T)
+        return jnp.matmul(pHpg(v2),v1)
+        
+    
+    return H_u, Hadjoint_to_gadjoint
+    
 
-        self.Hmatrix = diagmatrix + offdiagmatrix + randommatrix
 
-    def H(self, v):
-        """
-        The sparse Hamiltonian 
-        Written in a function representation
-        Aka, a "sparse" linear transformation that
-        recieves a vector v and returns another vector
-        """
-        resultv = v * self.diag_elements - self.g * v[self.flips_basis].sum(axis=1)
-        return resultv
 
-    def Hadjoint_to_gadjoint(self, v_1, v_2):
-        """
-        A function that receive the adjoint of the matrix H
-        as input, and return the adjoint of g as output.
-        """
-        return jnp.matmul(self.pHpg(v_2),v_1)[None]
+
 
 if __name__ == "__main__":
     N = 10
     g = 0.5
-    model = TFIM(N,g)
 
-    print("testing method 'setpHpg'\n")
-    model.setpHpg()
-    print("Done\n")
-
-    print("testing method 'setHmatrix'\n")
-    model.setHmatrix()
-    print("Done\n")
-
-    print("testing method 'pHpg'\n")
-    pHpg_matrix = model.pHpgmatrix
-    v = jnp.array(np.random.randn(pHpg_matrix.shape[0]))
-    resultv = model.pHpg(v)
-    print(f"Done with return {resultv}\n")
-
-    print("testing method 'H'\n")
-    pHpg_matrix = model.pHpgmatrix
-    v = jnp.array(np.random.randn(pHpg_matrix.shape[0]))
-    resultv = model.H(v)
-    print(f"Done with return {resultv}\n")
-
-    print("testing method 'Hadjoint_to_gadjoint'\n")
-    pHpg_matrix = model.pHpgmatrix
-    v_1 = jnp.array(np.random.randn(pHpg_matrix.shape[0]))
-    v_2 = jnp.array(np.random.randn(pHpg_matrix.shape[0]))
-    resultv = model.Hadjoint_to_gadjoint(v_1,v_2)
-    print(f"Done with return {resultv}\n")
-
-    print("Tests passed!")
 
 
 

chiF_Hv_product/fid_sus_sparse_representation.py
@@ -11,11 +11,10 @@ rather than full matrix form
 import jax.numpy as jnp
 from jax import jit, grad,lax
 import numpy as np
-from TFIM_init import TFIM
-import datetime
+from TFIM_init import H_u_initialize
 
 
-def from_g_to_logproduct_sparse(g,g_no_diff, N, k):
+def from_g_to_logproduct_sparse(g, N, k):
     """
     Computing logproduct using DominantSymeig in symeig.py,
     which is to perform AD with direct matrix form.
@@ -33,12 +32,9 @@ def from_g_to_logproduct_sparse(g,g_no_diff, N, k):
     """
     from symeig import DominantSparseSymeig
 
-    model = TFIM(N,g)
-    # model.setHmatrix()
-    hamiltonian = model.H
+    H_u, Hadjoint_to_gadjoint = H_u_initialize(N)
     dim = 2**N
-    Aadjoint_to_gadjoint = model.Hadjoint_to_gadjoint
-    energy_0, psi_0 = DominantSparseSymeig(Aadjoint_to_gadjoint ,hamiltonian,g,k,dim)
+    energy_0, psi_0 = DominantSparseSymeig(Hadjoint_to_gadjoint ,H_u,g,k,dim)
     psi_matmul = jnp.matmul(lax.stop_gradient(psi_0),psi_0)
     log_product = jnp.log(psi_matmul)
     return log_product
@@ -49,19 +45,18 @@ def fid_sus_sparse_repre(g,N,k):
         Computing fidelity susceptibility using DominantSparseSymeig in symeig.py,
     which is to perform AD with sparse representation of matrix A.
     """
-    g_diff = g
-    g_no_diff = g
-    dlogdg = grad(from_g_to_logproduct_sparse)
-    d2logdg = grad(dlogdg)(g_diff,g_no_diff,N,k)
+    dlogdg = grad(from_g_to_logproduct_sparse,argnums=0)
+    d2logdg = grad(dlogdg,argnums=0)(g,N,k)
     fid_sus = - d2logdg
     return fid_sus
+    
 
 
 
 if __name__ == "__main__":
     N = 10
     k = 300
-    g_count = 5
+    g_count = 200
     gs = np.linspace(0.5, 1.5, num = g_count)
     fid_sus_from_sparse_repre = np.empty(g_count)
     